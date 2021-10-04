b6d227dea987bd27bdf2ca91f3745abcf777c909
Emon <Emon@DESKTOP-98TRJS2>
2021-10-04 11:07:54 +0600

    Add project files.

---
+++ CQRS_with_MediatR/Context/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using CQRS_with_MediatR.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRS_with_MediatR.Context
+{
+    public class ApplicationContext : DbContext, IApplicationContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        { }
+        public async Task<int> SaveChanges()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
---
+++ CQRS_with_MediatR/Context/IApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using CQRS_with_MediatR.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace CQRS_with_MediatR.Context
+{
+    public interface IApplicationContext
+    {
+        DbSet<Product> Products { get; set; }
+
+        Task<int> SaveChanges();
+    }
+}
